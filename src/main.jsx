537cd4475edf765924b232cffb12f71c6735856a
yimeric7 <112520475+yimeric7@users.noreply.github.com>
2022-12-04 22:15:10 -0600

    routing system

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
 // Make Boggle svg in index.html & change title
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
   </React.StrictMode>
 )
