963f82900117810aaf9f11eb39f8435479a77665
yimeric7 <112520475+yimeric7@users.noreply.github.com>
2022-12-05 16:59:35 -0600

    Selection screens (#4)
    * Began implementing react screens. Included single/multiplayer mode select, multiplayer mode select, and multiplayer host setup screens.
    
    * Began validation for port input
    
    * Restructured file organization, added multiplayer join screen
    
    Co-authored-by: jjankovich2 <jjankovich@huskers.unl.edu>

---
+++ src/pages/MultiplayerSelectMode.jsx
@@ -0,0 +1,18 @@
+import '../styles/MultiplayerSelectMode.css'
+
+function MultiplayerSelectMode() {
+    return (
+        <div id="mutiplayer-select-mode-parent">
+            <div>
+                <h1>Select Multiplayer Mode</h1>
+                <h2>Do you want to join an existing game or host a new game?</h2>
+            </div>
+            <div id="mutiplayer-select-mode-section">
+                <button className="select-host-join-buttons" id="select-join" onClick={() => console.log("join")}>Join</button>
+                <button className="select-host-join-buttons" id="select-host" onClick={() => console.log("host")}>Host</button>
+            </div>
+        </div>
+    )
+}
+
+export default MultiplayerSelectMode
